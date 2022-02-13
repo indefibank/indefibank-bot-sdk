45d3d6ef1c5001f6396bc25f6e0c6ea1f629443d
velerofinance <imdipperpines3199@gmail.com>
2022-02-13 17:28:18 +0300

    update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,14 @@ setup(
     packages=[
         'velero_bot_sdk',
     ],
-    url='https://github.com/velerofinance/velero-velero-bot-sdk.git',
+    url='https://github.com/velerofinance/velero-bot-sdk.git',
     license='',
     author='velerofinance',
     author_email='imdipperpines3199@gmail.com',
     description='',
     install_requires=[
-        'httplib2==0.20.4',
-        'oauth2client~=4.1.3',
-        'google-api-python-client==2.36.0',
-        'tornado==6.1',
-        'web3==5.27.0',
-        'pymongo==4.0.1',
-        'gunicorn==20.1.0',
+        'web3==5.28.0',
+        'hexbytes~=0.2.2',
+        'requests~=2.27.1',
     ],
 )
