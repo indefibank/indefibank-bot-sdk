b59a9eeab49395688cfd496956e60a73048ea701
velerofinance <imdipperpines3199@gmail.com>
2022-02-13 18:29:50 +0300

    add default abi dir

--- velero_bot_sdk/__init__.py
+++ velero_bot_sdk/__init__.py
@@ -3,7 +3,7 @@ Velero Bot SDK
 ~~~~~~~~~~~~~~~~~~~~~
 :copyright: (c) 2022 by velerofinance
 """
-
+from pathlib import Path
 
 from velero_bot_sdk.contracts.dss import DssContractsConnector
 from velero_bot_sdk.contracts.wagyu import WagyuContractConnector
@@ -11,9 +11,13 @@ from velero_bot_sdk.contracts.utils import calc_perc, Converter
 from velero_bot_sdk.calulations.formuls import VeleroFormuls
 
 
+VELERO_DEFAULT_ABI_DIR = Path(__file__).parent / "contracts" / "abi"
+
+
 __all__ = [
     'DssContractsConnector',
     'WagyuContractConnector',
     'calc_perc',
     'Converter',
+    'VELERO_DEFAULT_ABI_DIR',
 ]
